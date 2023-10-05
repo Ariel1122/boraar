87c8bc11347c013ec1ccece075567b6d3b5921fb
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2023-10-05 06:21:40 -0500

    pelota que rebota clase

---
+++ sketch.js
@@ -0,0 +1,57 @@
+// let pelotas = [];
+let pel1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // for (let i = 0; i < 5; i++) {
+  //   // Puedes ajustar el número de pelotas que deseas
+  //   pelotas.push(new Pelota());
+  // }
+  pel1 = new Pelota(100, 200);
+}
+
+function draw() {
+  background(0, 200, 150, 70);
+
+  // for (let pelota of pelotas) {
+  //   pelota.update();
+  //   pelota.mostrar();
+  // }
+  pel1.update();
+  pel1.display();
+}
+
+class Pelota {
+  constructor() {
+    this.posX = windowWidth;
+    this.posY = windowHeight * 0.2;
+    this.diam = random(10, 50);
+    this.rad = this.diam / 2;
+    this.velY = 0;
+    this.acel = 0.98;
+    this.margen = 50;
+    this.esp = 34;
+    this.piso = height - this.margen - this.esp / 2;
+  }
+
+  update() {
+    this.velY += this.acel;
+    this.posY += this.velY;
+
+    if (this.posY >= this.piso - this.rad) {
+      print("ya");
+      this.posY = this.piso - this.rad;
+      this.velY *= -1;
+    }
+  }
+
+  display() {
+    noStroke();
+    fill(255);
+    circle(this.posX, this.posY, this.diam);
+
+    fill(255, 0, 0);
+    rect(windowWidth / 2, windowHeight - this.margen, windowWidth, this.esp);
+    // ES NECESARIO EL WINDOW?
+  }
+}
